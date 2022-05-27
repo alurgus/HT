4336a4e9b5d0e8e2f8b42c9088af8d08c5bdd687
Alurgus <alurgus@mail.ru>
2022-05-27 18:50:37 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+const goods = [
+    { title: 'Shirt', price: 150 },
+    { title: 'Socks', price: 50 },
+    { title: 'Jacket', price: 350 },
+    { title: 'Shoes', price: 250 },
+];
+
+class GoodsItem {
+    constructor({ title, price }) {
+        this.title = title;
+        this.price = price;
+    }
+    render() {
+        return `
+      <div class="goods-item">
+        <h3>${this.title}</h3>
+        <p>${this.price}</p>
+      </div>
+    `;
+    }
+}
+class GoodsList {
+    items = [];
+    fetchGoods() {
+        this.items = goods;
+    }
+    calculatePrice() {
+        let result = 0;
+        this.items.forEach(({ price }) => {
+            result += price;
+        })
+        return (price);
+    }
+    render() {
+        const goods = this.items.map(item => {
+            const goodItem = new GoodsItem(item);
+            return goodItem.render()
+        }).join('');
+
+        document.querySelector('.goods-list').innerHTML = goods;
+    }
+}
+
+const goodsList = new GoodsList();
+goodsList.fetchGoods();
+goodsList.render();
